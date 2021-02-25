200c117e2b1e1afe121cc8fa0177babd512841e4
tulon <reashedzamiltulon@gmail.com>
2021-02-25 22:00:52 +0600

    Initial Commit

---
+++ Tamo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamo
+//
+//  Created by Reashed Tulon on 25/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
